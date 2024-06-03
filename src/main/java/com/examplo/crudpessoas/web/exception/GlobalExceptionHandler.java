05d6df9d278b7d9c6b8a0466a050fa76216ad798
Thainara Onofre <thainaraonofre@outlook.com>
2024-06-03 15:32:36 -0300

    Implementando ExcepetionHandler

---
+++ src/main/java/com/examplo/crudpessoas/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.examplo.crudpessoas.web.exception;
+
+
+import com.examplo.crudpessoas.exception.CPFInvalido;
+import com.examplo.crudpessoas.exception.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(CPFInvalido.class)
+    public ResponseEntity<ErrorMessage> handleCPFInvalidoException(RuntimeException ex, HttpServletRequest request) {
+        log.error("Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage()));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handleResourceNotFoundException(RuntimeException ex, HttpServletRequest request) {
+        log.error("Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
+                                                                              HttpServletRequest request,
+                                                                              BindingResult result) {
+        log.error("Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campo(s) invalido(s)", result));
+    }
+}
